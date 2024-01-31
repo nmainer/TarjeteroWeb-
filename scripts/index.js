565a2515bd9822a7c0a589f8c6c7635c5520b54d
nmainer <mainermnicolas@gmail.com>
2024-01-31 19:15:39 -0300

    commit4

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,9 @@
 
 class Activity {
     constructor (id,title,description,imgUrl) {
-        this.id= id;
-        this.title =title;
-        this.description = description;
-        this.imgUrl= imgUrl
-
+      this.id = id ,
+      this.title = title,
+      this.description =description,
+      this.imgUrl = imgUrl
     }
 }
-
-
-class Repository {
-    constructor () {
-     this.activities = [];
-    }
-    getAllActivities () {
-        return this.activities;
-    }
-    createActivity(id,title,description,imgUrl) {
-        const instancia = new Activity (id,title,description,imgUrl)
-        this.activities.push(instancia)
-        return this.activities;
-    }
-    deleteActivity(id){
-      this.activities = this.activities.filter ((activities) => activities.id !== id )
-    }
-}
-

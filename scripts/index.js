8bf886f99ece477d9897d5fd05a3ea0e2a441d83
nmainer <mainermnicolas@gmail.com>
2024-01-31 20:43:44 -0300

    commit5

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@ class Activity {
       this.imgUrl = imgUrl
     }
 }
+
+class Repository {
+    constructor () {
+        this.activities = [];
+    }
+    getAllActivities = () => this.activities
+    createActivity = (id,title,description,imgUrl) => {
+       let valor = new Activity (id,title,description,imgUrl)
+       this.activities.push(valor)
+    }
+    deleteActivity  (id) {
+        let valor1 = this.activities.filter((act) => act.id !== id)
+        this.activities = valor1
+    }
+}
+
+
+
+
+
+
+
+
+

fce284617c0d15faa70f34d8d1850759e68fd118
nmainer <mainermnicolas@gmail.com>
2024-03-21 01:01:16 -0300

    21/03/24

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,37 @@
 
+//const {Activity,Repository}= require("../scripts/modelos");
 class Activity {
-    constructor ({id,title,description,imgUrl}) {
-      this.id = id ,
-      this.title = title,
-      this.description =description,
-      this.imgUrl = imgUrl
-    }
+  constructor ({id,title,description,imgUrl}) {
+    this.id = id ,
+    this.title = title,
+    this.description =description,
+    this.imgUrl = imgUrl
+  }
 }
 
 class Repository {
-    constructor () {
-        this.activities = [];
-        this.id=0;
-    }
-    getAllActivities () {
-     return  this.activities;
-    } 
-    createActivity  (obj)  {
-        this.id=this.id+1;
-       let valor = new Activity ({...obj,id:this.id});
-       this.activities.push(valor)
-    };
-    deleteActivity (id) {
-        let valor1 = this.activities.filter(function (act){
-            return act.id !==id;
-        })
-      this.activities = valor1;
-       
-    };
+  constructor () {
+      this.activities = [];
+      this.id=0;
+  }
+  getAllActivities () {
+   return  this.activities;
+  } 
+  createActivity  (obj)  {
+      this.id=this.id+1;
+     let valor = new Activity ({...obj,id:this.id});
+     this.activities.push(valor)
+  };
+  deleteActivity (id) {
+      let valor1 = this.activities.filter(function (act){
+          return act.id !==id;
+      })
+    this.activities = valor1;
+     
+  };
 
 };
+
 
 let valor = new Repository();
 
@@ -122,8 +124,8 @@ let boton = document.querySelector("#submit");
 
 
 
-boton.addEventListener("click", function (evento){
-  evento.preventDefault();
+boton.addEventListener("click", function (event){
+  event.preventDefault();
   handler();
 });
 
--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,26 @@
-describe("demo", function () {
-  it("Este test debe pasar siempre", function () {
-    expect(4 + 2).toBe(6);
+const {Activity,Repository} = require ("../scripts/modelos");
+
+describe("pruebas sobre la clase Repository", function () {
+  afterEach( ()=>{
+    elemento = new Repository();
+  })
+
+  it("Repository debe ser una clase", function () {
+    expect(typeof Repository.prototype.constructor).toBe("function");
   });
+  it ("elemento debe ser una instancia de Repository" , ()=>{
+    expect( elemento instanceof Repository).toBe(true)
+  })
+  it ("Repository debe poseer el metodo getAllActivities",()=>{
+    expect(elemento.getAllActivities).toBeDefined()
+  })
+  it ("Repository debe poseer el metodo createActivity",()=>{
+    expect(elemento.createActivity).toBeDefined()
+  })
+  it ("Repository debe poseer el metodo deleteActivity",()=>{
+    expect(elemento.deleteActivity).toBeDefined()
+  })
+
 });
+
+
